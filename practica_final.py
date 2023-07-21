746e3d780b33af59db626912349375c08b6b79f8
marinaredom23 <marinaredom23@gmail.com>
2023-07-21 18:27:11 +0200

    inicio

---
+++ practica_final.py
@@ -0,0 +1,7 @@
+'Escribir un programa en Python que genere una matriz de tamaño NxN y la llene con números aleatorios entre 0 y 9. '
+from random import*
+
+N=input("Por favor ingrese el tamaño de la matriz:  ")
+N=int(N)
+
+
